fd657c69926561d1616e2d1579cd3d2e20cbd3ba
shubham775 <ssp797@outlook.com>
2016-01-14 19:44:21 +0530

    Create inputModule.py

---
+++ inputModule.py
@@ -0,0 +1,16 @@
+'''
+When input file is in the format -- see the example
+
+Sample Input :
+4
+12 43 65 76
+
+in below code the n will store the value 4 and the array will be stored in arr
+'''
+
+
+import sys
+
+n = int(raw_input().strip())
+arr = map(int,raw_input().strip().split(' '))
+
